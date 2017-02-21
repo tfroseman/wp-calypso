15cbdcb38a64bfd0df5e7247896798f5062e1b78
Matías Ventura <mv@matiasventura.com>
2017-02-21 16:43:18 +0100

    Pushing changes.

--- client/boot/index.js
+++ client/boot/index.js
@@ -137,7 +137,6 @@ function loadDevModulesAndBoot() {
 }
 
 function boot() {
-	var localeSlug;
 	const project = config( 'project' );
 
 	init();
@@ -146,40 +145,7 @@ function boot() {
 		require( './' + project );
 	}
 
-	// When the user is bootstrapped, we also bootstrap the
-	// locale strings
-	if ( ! config( 'wpcom_user_bootstrap' ) ) {
-		localeSlug = user.get().localeSlug;
-		if ( localeSlug ) {
-			switchLocale( localeSlug );
-		}
-	}
-	// Set the locale for the current user
-	user.on( 'change', function() {
-		localeSlug = user.get().localeSlug;
-		if ( localeSlug ) {
-			switchLocale( localeSlug );
-		}
-	} );
-
-	translatorJumpstart.init();
-
 	createReduxStoreFromPersistedInitialState( reduxStoreReady );
-}
-
-function renderLayout( reduxStore ) {
-	const Layout = require( 'controller' ).ReduxWrappedLayout;
-
-	const layoutElement = React.createElement( Layout, {
-		store: reduxStore
-	} );
-
-	ReactDom.render(
-		layoutElement,
-		document.getElementById( 'wpcom' )
-	);
-
-	debug( 'Main layout rendered.' );
 }
 
 function reduxStoreReady( reduxStore ) {
@@ -362,8 +328,6 @@ function reduxStoreReady( reduxStore ) {
 			}
 		} );
 	}
-
-	require( 'my-sites' )();
 
 	// clear notices
 	page( '*', function( context, next ) {
