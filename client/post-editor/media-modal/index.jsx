bf85d3925be99f9eda02d8da151f4bb23a2c3fd7
Donna Peplinskie <donnapep@gmail.com>
2017-02-21 14:29:32 -0500

    Add support for selecting a frame of the video to be the poster

--- client/post-editor/media-modal/index.jsx
+++ client/post-editor/media-modal/index.jsx
@@ -235,6 +235,8 @@ export class EditorMediaModal extends Component {
 		this.props.setView( ModalViews.DETAIL );
 	};
 
+	handleUpdatePoster = () => this.props.setView( ModalViews.DETAIL );
+
 	handleCancel = () => {
 		const { mediaLibrarySelectedItems } = this.props;
 		const item = mediaLibrarySelectedItems[ this.getDetailSelectedIndex() ];
@@ -425,6 +427,7 @@ export class EditorMediaModal extends Component {
 						<VideoEditor
 							media={ media }
 							onCancel={ this.handleCancel }
+							onUpdatePoster={ this.handleUpdatePoster }
 						/>
 					);
 				}
