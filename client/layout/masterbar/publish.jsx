b7804162fa1141ddb0d15ce480d2ac62a3f7006b
Matías Ventura <mv@matiasventura.com>
2017-02-23 20:29:31 +0100

    Masterbar: add recent drafts popover contextual to current site.
    This gives easy access to latest posts you are working on in a
    consistent place in the navigation.

--- client/layout/masterbar/publish.jsx
+++ client/layout/masterbar/publish.jsx
@@ -14,6 +14,7 @@ import paths from 'lib/paths';
 import viewport from 'lib/viewport';
 import { preload } from 'sections-preload';
 import { getSelectedSite } from 'state/ui/selectors';
+import AsyncLoad from 'components/async-load';
 
 const MasterbarItemNew = React.createClass( {
 	propTypes: {
@@ -28,7 +29,8 @@ const MasterbarItemNew = React.createClass( {
 
 	getInitialState() {
 		return {
-			isShowingPopover: false
+			isShowingPopover: false,
+			showDrafts: false
 		};
 	},
 
@@ -65,32 +67,47 @@ const MasterbarItemNew = React.createClass( {
 		return 'bottom left';
 	},
 
+	toggleDrafts( event ) {
+		event.stopPropagation();
+		event.preventDefault();
+		this.setState( {
+			showDrafts: ! this.state.showDrafts
+		} );
+	},
+
+	closeDrafts() {
+		this.setState( { showDrafts: false } );
+	},
+
 	render() {
 		const classes = classNames( this.props.className );
 		const currentSite = this.props.selectedSite || this.props.user.get().primarySiteSlug;
 		const newPostPath = paths.newPost( currentSite );
 
 		return (
-			<MasterbarItem
-				ref={ this.setPostButtonContext }
-				url={ newPostPath }
-				icon="create"
-				onClick={ this.onClick }
-				isActive={ this.props.isActive }
-				tooltip={ this.props.tooltip }
-				className={ classes }
-				preloadSection={ () => preload( 'post-editor' ) }
-			>
-				{ this.props.children }
-				<SitesPopover
-					id="popover__sites-popover-masterbar"
-					sites={ this.props.sites }
-					visible={ this.state.isShowingPopover }
-					context={ this.state.postButtonContext }
-					onClose={ this.toggleSitesPopover.bind( this, false ) }
-					groups={ true }
-					position={ this.getPopoverPosition() } />
-			</MasterbarItem>
+			<div className="masterbar__publish">
+				<MasterbarItem
+					ref={ this.setPostButtonContext }
+					url={ newPostPath }
+					icon="create"
+					onClick={ this.onClick }
+					isActive={ this.props.isActive }
+					tooltip={ this.props.tooltip }
+					className={ classes }
+					preloadSection={ () => preload( 'post-editor' ) }
+				>
+					{ this.props.children }
+					<SitesPopover
+						id="popover__sites-popover-masterbar"
+						sites={ this.props.sites }
+						visible={ this.state.isShowingPopover }
+						context={ this.state.postButtonContext }
+						onClose={ this.toggleSitesPopover.bind( this, false ) }
+						groups={ true }
+						position={ this.getPopoverPosition() } />
+				</MasterbarItem>
+				<AsyncLoad require="layout/masterbar/drafts" />
+			</div>
 		);
 	}
 } );
